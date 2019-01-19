20a7fb5e51ff692b872ff3392d0c97eb7bb7de7b
MJeannette <mndayisenga2015.s@gmail.com>
2019-01-19 13:01:07 +0200

    added a function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // business logic
-
+var newAccount;
+var balance;
 function Create(first, last, phone, id, date, amount) {
   this.firstName = first;
   this.lastName = last;
@@ -12,6 +13,7 @@ Create.prototype.fullName = function() {
   return this.initialAmount;
   // return this.firstName + "  " + this.lastName + "  " + this.initialAmount;
 };
+
 // user interface logic
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
@@ -19,13 +21,12 @@ $(document).ready(function() {
     $("#depo").show();
     $("#withd").show();
     event.preventDefault();
-
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhone = $("input#new-phone-number").val();
     var inputtedId = $("input#new-id").val();
     var inputtedDate = $("input#new-date").val();
-    var inputtedAmount = $("input#new-amount").val();
+    var inputtedAmount = parseInt($("input#new-amount").val());
     var newCreate = new Create(
       inputtedFirstName,
       inputtedLastName,
@@ -34,11 +35,20 @@ $(document).ready(function() {
       inputtedDate,
       inputtedAmount
     );
+    var bb = newCreate.amount;
     // console.log(newCreate);
     // $("ul#contacts").append(
     //   "<li><span class='create'>" + newCreate.fullName() + "</span></li>"
     // );
     $("#displ").text(newCreate.fullName());
+    $("form#depo").submit(function(event) {
+      event.preventDefault();
+      var deposit = parseInt($("input#new-amount1").val());
+      console.log(deposit);
+      console.log(newCreate.amount);
+      balance = bb + deposit;
+      $("#displ").text(balance);
+    });
 
     // $(".create")
     //   .last()
@@ -61,4 +71,3 @@ $(document).ready(function() {
     //   });
   });
 });
-user;
